d31f90d4722b2f86f93ced276d238375bff38c07
amit.asila <tokopedia@amitasila-mbp.local>
2022-01-15 07:44:00 +0530

    Header added

---
+++ src/pages/ErrorGame/pages/MemoryGame/Header/index.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+
+function Header() {
+  return (
+    <div>
+      <div className="searchbar-wrapper">
+      <a href="https://www.tokopedia.com" className="logo">
+          <img alt="tokopedia-logo" src="https://ecs7.tokopedia.net/assets-tokopedia-lite/v2/zeus/production/e5b8438b.svg"/>
+      </a>
+      <div className="buttonWrapper">
+          <Link to='/error/2' type="button" className="register">Next Game</Link>
+      </div>
+        </div>
+    </div>
+  )
+}
+
+export default Header
--- src/pages/ErrorGame/pages/MemoryGame/index.js
+++ src/pages/ErrorGame/pages/MemoryGame/index.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import View from './Components/View';
 import './style.css';
+import Header from './Header';
 
 function Memory() {
   return (
     <div>
+        <Header />
         <View />
     </div>
   )
